7a1c45f3d4ef9c79e5d4bbddddeff8eb2088355e
Vivek5501 <2003viv10@gmail.com>
2024-11-12 07:37:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import DashBoard from './components/DashBoard/DashBoard';
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAllData } from './Actions/DataAction';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { allTickets } = useSelector(state => state.DataReducer);
+  useEffect(() => {
+    dispatch(fetchAllData());
+  }, [dispatch])
+  return(
+    <div style={{ paddingTop: "10px" }} >
+      <NavBar />
+      <hr style={{ marginTop: "10px" }} />
+      <DashBoard />
+    </div>
+  )
+}
+export default App
